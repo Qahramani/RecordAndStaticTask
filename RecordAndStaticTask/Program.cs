d55d73ee4405d1827714fa7630574fd5cfca57f0
testtttttttttt1234 <wapigix358@bsomek.com>
2024-06-12 11:54:25 +0400

    Add project files.

---
+++ RecordAndStaticTask/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace RecordAndStaticTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User[] users = new User[3];
+            for (int i = 0; i < users.Length; i++)
+            {
+                Console.WriteLine($"User {i + 1}");
+                users[i] = CreateUser();
+            }
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("1.Show all students");
+                Console.WriteLine("2. Get info by id");
+                Console.WriteLine("0.Quit");
+                Console.Write("Enter operation : ");
+                string opt = Console.ReadLine();
+                if (opt == "1")
+                {
+                    foreach (var user in users)
+                    {
+                        Console.WriteLine(user.GetInfo());
+                    }
+                }
+                else if (opt == "2")
+                {
+                    Console.Write("Enter Id to get User : ");
+                    int id = int.Parse(Console.ReadLine());
+                    Console.WriteLine(User.FindUserById(id, users));
+                }
+                else if (opt == "0")
+                {
+                    break;
+                }
+                else
+                    Console.WriteLine("Please enter valid operation!");
+                Console.ReadLine();
+            }
+        }
+
+        public static User CreateUser()
+        {
+            Console.Write("Please enter your Full Name : ");
+            string fullname = Console.ReadLine();
+            Console.Write("Please enter your Email : ");
+            string email = Console.ReadLine();
+            string pw;
+            while (true)
+            {
+                Console.Write("Please enter your Password : ");
+                pw = Console.ReadLine();
+                if (User.PasswordChecker(pw))
+                    break;
+                Console.WriteLine("Please enter valid password!");
+            }
+            
+            return new User(email, pw, fullname);
+        }
+    }
+}
---
+++ RecordAndStaticTask/User.cs
@@ -0,0 +1,53 @@
+﻿namespace RecordAndStaticTask
+{
+    public class User
+    {
+
+        private static int _id;
+        public readonly int Id;
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public User(string email, string password, string fullname)
+        {
+            Email = email;
+            Password = password;
+            FullName = fullname;
+            Id = ++_id;
+        }
+
+        public static bool PasswordChecker(string pw)
+        {
+            bool ContainsUpper = false;
+            bool ContainsLower = false;
+            bool ContainsDigit = false;
+
+            for (int i = 0; i < pw.Length; i++)
+            {
+                if (char.IsUpper(pw[i]))
+                    ContainsUpper = true;
+                if (char.IsLetter(pw[i]))
+                    ContainsLower = true;
+                if (char.IsDigit(pw[i]))
+                    ContainsDigit = true;
+            }
+
+            return ContainsUpper && ContainsLower && ContainsDigit && pw.Length >= 8;
+        }
+
+        public string GetInfo()
+        {
+            return $"FullName : {FullName}, Id : {Id} , Email : {Email}";
+        }
+
+        public static string FindUserById(int id, User[] users)
+        {
+            foreach (var user in users)
+            {
+                if (id == user.Id)
+                    return $"FullName : {user.FullName}, Id : {user.Id} , Email : {user.Email}";
+            }
+            return "No user with given Id";
+        }
+    }
+}
